4ae97496532c86551346a79b7b8a833c49eccc07
Maxat Uatbayev <maksatuatbayev@gmail.com>
2020-05-21 17:24:32 +0600

    Добавьте файлы проекта.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Task1_1.Models;
+
+namespace Task1_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Welcomepage()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Доброе утро" : "Добрый день";
+            return View("Welcomepage");
+        }
+        [HttpGet]
+        public ActionResult Form()
+        {
+            return View("Form");
+        }
+        [HttpPost]
+        public ActionResult Form(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+
+                return View();
+            }
+        }
+        public ActionResult ListResponses()
+        {
+            return View(Repository.Responses);
+        }
+
+
+
+    }
+}
---
+++ Models/GuestResponse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Task1_1.Models
+{
+    public class GuestResponse
+    {
+       
+      
+            [Required(ErrorMessage = "Заполните поле имя")]
+            public string Name { get; set; }
+
+            [Required(ErrorMessage = "Заполните поле email")]
+            public string Email { get; set; }
+
+            [Required(ErrorMessage = "Заполните поле Номер телефона")]
+            public string Phone { get; set; }
+
+        }
+    }
+
