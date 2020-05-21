59945ab16b85fa15ba3bd81f8699bcd6e0664650
Maxat Uatbayev <maksatuatbayev@gmail.com>
2020-05-21 17:46:13 +0600

    Add AnswersDb

---
+++ Models/Answers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+namespace Task1_1.Models
+{
+    public class AnswersContext : DbContext
+    { public DbSet<GuestResponse> GuestResponses { get; set; }
+        public AnswersContext(DbContextOptions<AnswersContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+ 
+}
+    
+}
--- Startup.cs
+++ Startup.cs
@@ -1,15 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Task1_1.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
 
-namespace Task1_1
+namespace EFDataApp
 {
     public class Startup
     {
@@ -20,13 +17,16 @@ namespace Task1_1
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+       
+            string connection = Configuration.GetConnectionString("DefaultConnection");
+            
+            services.AddDbContext<AnswersContext>(options =>
+                options.UseSqlServer(connection));
             services.AddControllersWithViews();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -36,7 +36,6 @@ namespace Task1_1
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
